cdffebac54eba843e0230ab18e1dee248b6accb9
Oscar Perona <144930013+peronaOscar@users.noreply.github.com>
2023-11-29 19:12:47 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -51,6 +51,63 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.floorDark0,sprites.dungeon.hazardLava1,sprites.vehicle.roadIntersection3,sprites.vehicle.roadIntersection2,sprites.vehicle.roadIntersection1,sprites.vehicle.roadTurn1,sprites.vehicle.roadIntersection4,sprites.vehicle.roadTurn4,sprites.dungeon.doorLockedSouth,sprites.dungeon.greenOuterNorth0,sprites.dungeon.stairLarge,sprites.dungeon.floorDark2,sprites.builtin.oceanSand8,sprites.swamp.swampTile16,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenInnerSouthEast,myTiles.tile1,myTiles.tile3,myTiles.tile4,myTiles.tile5], TileScale.Sixteen);
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level4":
+            case "level4":return tiles.createTilemap(hex`100010002221010527231a1e1e1e1f1c1b1c1b1c13090105090f0d0d250d0d0d0a1b1b1b132701050909270909090926231b1b1b1327010507070707070704090b1c1d151326020303030303030305260b1d18182209262709260909260105090b1918180e1109080707070707070526231a1e1e1712090103030303030306090f0d0d0a1622270105260909092609090909210b161309010509260807070707070409231613090105090901030303030305270b1613090105200901052609090105090b1f13270105070701050923090105090b1c1309020303030306090c270105260b1b1209092709090909270c090105260b1c0e2410101010101024140901050923`, img`
+2 2 . . . 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . 2 2 2 2 
+2 . . . . . . . . . . . 2 2 2 2 
+2 . . . . . . . . . . . 2 2 2 2 
+2 . . . . . . . . . . . 2 2 2 2 
+2 2 . . . . . . . . . . 2 2 2 2 
+2 2 . . . . . . . . . . 2 2 2 2 
+2 2 . . . . . . . . . . . . 2 2 
+2 2 . . . . . . . . . . . . . 2 
+2 2 . . . . . . . . . . . . . 2 
+2 2 . . . 2 . . . . . . . . . 2 
+2 2 . . . . . . . . 2 . . . . 2 
+2 2 . . . . . . . . 2 . . . . 2 
+2 2 . . . . . . . . 2 . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 . . . . 2 
+`, [myTiles.transparency16,sprites.vehicle.roadIntersection2,sprites.vehicle.roadTurn3,sprites.vehicle.roadIntersection1,sprites.vehicle.roadTurn2,sprites.vehicle.roadIntersection4,sprites.vehicle.roadTurn4,sprites.vehicle.roadIntersection3,sprites.vehicle.roadTurn1,sprites.dungeon.floorDark0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.hazardLava1,sprites.dungeon.hazardLava0,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.chestOpen,sprites.dungeon.chestClosed,sprites.dungeon.greenOuterWest2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.floorDark4,sprites.dungeon.floorDark1], TileScale.Sixteen);
+            case "level5":
+            case "level5":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
         }
         return null;
     })
